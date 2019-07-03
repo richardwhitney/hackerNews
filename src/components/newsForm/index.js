fa4539886aefcc42b9ec37b5a14de721ae4129f2
Richard Whitney <rwhitney32@gmail.com>
2019-07-03 20:26:57 +0100

    NewsForm component completed

---
+++ src/components/newsForm/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './newsForm.css';
+
+export default class Form extends Component {
+  render() {
+    return (
+      <form className="form bg-dark text-light">
+        <h3>Add a news item</h3>
+        <div className="form-group">
+          <input type="text" className="form-control" placeholder="Title"/>
+        </div>
+        <div className="form-group">
+          <input type="text" className="form-control" placeholder="Author"/>
+        </div>
+        <div className="form-group">
+          <input type="text" className="form-control" placeholder="Link"/>
+        </div>
+        <button type="submit" className="btn btn-primary">Add</button>
+      </form>
+    );
+  }
+}
--- stories/index.js
+++ stories/index.js
@@ -1,3 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import NewsForm from '../src/components/newsForm';
+
+storiesOf("Hacker App/News Form", module).add("default", () => <NewsForm/>);
